e65208923053687b5a5e555eed8a70e6b4d7eff1
ricardobarroslourenco <ricardobarroslourenco@gmail.com>
2016-02-08 11:01:04 -0600

    First implementation of harvest index and decade variation for gridded data

--- ede/utils/op_base.py
+++ ede/utils/op_base.py
@@ -1,35 +1,32 @@
 import numpy as np
-from scipy import stats
 #import sklearn as sk
+from scipy import stats
+#from shapely import ops, geometry
+
 
 __author__ = "rlourenc@mail.depaul.edu"
 # 2016-02-05 - Initial commit
 # Implements base spatial operations for abstraction consumption.
-
+#Todo: Grid to poly operations must be specified on usecases prior to implementation
 
 class aggregation:
-    def grid_to_grid(self, grid1, grid2):
-        agg_grid = np.divide((np.matrix(grid1)+np.matrix(grid2)),2.)
+    def grid_to_grid(self, cube):
+        # Operation applied for a 3D cube. Cube is on EPSG order
+        # (0, 1, 2, being latitude, longitude, time), being collapsed on time.
+        # Null values will be discarded.
+        agg_grid = np.apply_over_axes(np.nanmean,cube,2)
         return agg_grid
 
-    def grid_to_poly():
+    def grid_to_poly(self):
         return
-
-    def poly_to_poly():
-        return
-    return
 
 class binning:
-    def grid_to_grid(self, grid1, grid2):
-        binned_grid = stats.
-        return binned_grid
+    def grid_to_grid(self, cube, bin_range):
+        binned_cube = np.apply_over_axes(np.histogram(bins=bin_range), cube, 2)
+        return binned_cube
 
-    def grid_to_poly():
+    def grid_to_poly(self):
         return
-
-    def poly_to_poly():
-        return
-    return
 
 class normalization:
     def grid_to_grid(self, grid1, grid2):
@@ -38,10 +35,5 @@ class normalization:
         norm_grid = np.divide(np.matrix(grid1),np.matrix(grid2))
         return norm_grid
 
-    def grid_to_poly():
+    def grid_to_poly(self):
         return
-
-    def poly_to_poly():
-        return
-    return
-
--- ede/utils/operations.py
+++ ede/utils/operations.py
@@ -1,5 +1,7 @@
 import op_base
+import datetime
 # TODO: PostGIS endpoint
+# Todo: Prior to sending GeoJSON messages, treat all NaN for Null (in adhrence to JSON standard)
 
 __author__ = "rlourenc@mail.depaul.edu"
 # 2016-02-05 - Initial commit
@@ -8,17 +10,21 @@ __author__ = "rlourenc@mail.depaul.edu"
 class abstraction:
 # Implements the user abstractions of spatial operations. Currently related to use cases.
 
-    def HarvestIndex(self):
+    def HarvestIndex(self, simulation, bounding_box, time_slice):
         # Implements the Harvest Index for a crop. HI = yield / biomass .
     # Todo: Make query for obtaining a 'yield matrix', and other for a 'biomass' matrix.
-        crop_yield = np.matrix()
+        crop_yield = np.matrix() # use on this call, simulation and bounding box
         crop_biomass = np.matrix()
-        hi_matrix = op_base.aggregation.grid_to_grid(crop_yield,crop_biomass)
+        hi_matrix = op_base.normalization.grid_to_grid(crop_yield,crop_biomass)
         return hi_matrix
 
-    def DecadeVariation(simulation, bounding_box, begin_year, end_year):
+    def DecadeVariation(self, simulation, bounding_box, begin_year, end_year):
         # Given a input crop, it returns its variation within decades. Begin and end years are optional (if not
         # supplied return the complete series)
     # Todo: Make query for obtaining the simulation cube
-
-        return
+        ts_cube = np.matrix() #using bounding box and simulation to call
+        begin = datetime.timedelta(begin_year, 1, 1)
+        end = datetime.timedelta(end_year, 1, 1)
+        bin_range = begin - end
+        bin_cube = op_base.binning.grid_to_grid(ts_cube, bin_range)
+        return bin_cube
