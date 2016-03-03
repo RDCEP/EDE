202287e2e05316227c93a9a86f48110fd2ec65ac
njmattes <matteson@obstructures.org>
2016-03-03 07:52:14 -0600

    New database settings

--- ede/database.py
+++ ede/database.py
@@ -1,23 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.pool import NullPool
-from sqlalchemy.ext.declarative import declarative_base
-from ede.settings import DATABASE_CONN
+from sqlalchemy import Column, Integer
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from ede_test.config import SQLALCHEMY_DATABASE_URI
 
-app_engine = create_engine(DATABASE_CONN, convert_unicode=True)
 
-task_engine = create_engine(
-    DATABASE_CONN,
-    convert_unicode=True,
-    poolclass=NullPool)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URI, convert_unicode=True,
+    pool_size=10)
+db_session = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
-session = scoped_session(sessionmaker(bind=app_engine,
-                                      autocommit=False,
-                                      autoflush=False))
 
-task_session = scoped_session(sessionmaker(bind=task_engine,
-                                      autocommit=False,
-                                      autoflush=False))
+class EDEBase(object):
+    id = Column(Integer, primary_key=True)
 
-Base = declarative_base()
-Base.query = session.query_property()
+    @declared_attr
+    def __tablename__(cls):
+        return re.sub(r'(.)([A-Z])', r'\1_\2', cls.__name__).lower()
+
+
+Base = declarative_base(cls=EDEBase)
+Base.query = db_session.query_property()
+db_session.session = db_session
