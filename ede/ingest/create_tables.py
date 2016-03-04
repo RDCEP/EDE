55404d242db55675219c395814d640653e066f4b
njmattes <matteson@obstructures.org>
2016-03-03 20:39:23 -0600

    Get db connection settings from credentials

--- ede/ingest/create_tables.py
+++ ede/ingest/create_tables.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from ede.schema.models import Base
+from ede.config import SQLALCHEMY_DATABASE_URI
 
 
 def main():
-    engine = create_engine('postgresql://postgres:postgres@localhost:5432/ede',
-                           echo=True)
+    engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
     Base.metadata.create_all(engine)
 
 if __name__ == "__main__":
