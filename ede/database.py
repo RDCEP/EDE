536cc0f9d4fdc93c960e555c9ae7b4dc3e92d0cb
legendOfZelda <sev.thaler@gmail.com>
2016-03-04 14:12:38 -0600

    removed EDEBase

--- ede/database.py
+++ ede/database.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import re
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer
 from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy.ext.declarative import declarative_base
 from ede.config import SQLALCHEMY_DATABASE_URI
 
 
@@ -14,11 +12,6 @@ engine = create_engine(
 db_session = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
-
-class EDEBase(object):
-    uid = Column(Integer, primary_key=True)
-
-
-Base = declarative_base(cls=EDEBase)
+Base = declarative_base()
 Base.query = db_session.query_property()
 db_session.session = db_session
--- ede/schema/models.py
+++ ede/schema/models.py
@@ -1,13 +1,12 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Index
+from ede.database import Base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import JSONB
 from geoalchemy2 import Raster
 from sqlalchemy.sql import func
 
-Base = declarative_base()
-
 class Grid_Meta(Base):
     __tablename__ = 'grid_meta'
+    uid = Column(Integer, primary_key=True)
     filename = Column(String)
     filesize = Column(Integer)
     filetype = Column(String)
@@ -17,10 +16,12 @@ class Grid_Meta(Base):
  
 class Grid_Var(Base):
     __tablename__ = 'grid_vars'
+    uid = Column(Integer, primary_key=True)
     vname = Column(String, nullable=False, unique=True, index=True)
    
 class Grid_Data(Base):
     __tablename__ = 'grid_data'
+    uid = Column(Integer, primary_key=True)
     meta_id = Column(Integer, ForeignKey('grid_meta.uid'))
     var_id = Column(Integer, ForeignKey('grid_vars.uid'))
     rast = Column(Raster)
