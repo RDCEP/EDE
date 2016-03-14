0f513b41e264a52790f4d58188721c0a9cf68a82
njmattes <matteson@obstructures.org>
2016-03-14 13:22:56 -0500

    Add polygon intersection

--- ede/sql/get_raster_centroids.sql
+++ ede/sql/get_raster_centroids.sql
@@ -1,12 +1,24 @@
-CREATE OR REPLACE FUNCTION get_raster_centroids(mid int, vid int, band int)
-RETURNS TABLE(x int, y int, st_astext text) AS $$
+CREATE TYPE raster_centroid AS (
+  x INT,
+  y INT,
+  st_astext TEXT
+);
+
+CREATE OR REPLACE FUNCTION get_raster_centroids(
+  mid INT, vid INT, north FLOAT, east FLOAT, south FLOAT, west FLOAT)
+RETURNS SETOF raster_centroid AS $$
+DECLARE
+  poly GEOMETRY;
 BEGIN
+  poly := polygon_from_bbox(north, east, south, west);
   RETURN QUERY
   SELECT r.x, r.y, ST_AsText(r.geom)
-  FROM (SELECT (ST_PixelAsCentroids(rast, band)).*
+  FROM (SELECT (ST_PixelAsCentroids(rast, 1, FALSE)).*
         FROM grid_data AS gd
         WHERE gd.meta_id = mid
               AND gd.var_id = vid
+              AND ST_Intersects(
+                rast, poly)
        ) AS r;
 END;
 $$ LANGUAGE plpgsql;
--- ede/sql/get_raster_values.sql
+++ ede/sql/get_raster_values.sql
@@ -1,9 +1,22 @@
-CREATE OR REPLACE FUNCTION get_raster_values(mid int, vid int)
-RETURNS TABLE(nband int, valarray double precision[][]) AS $$
+CREATE TYPE raster_values AS (
+  nband INT,
+  valarray FLOAT[][]
+);
+
+
+CREATE OR REPLACE FUNCTION get_raster_values(
+  mid INT, vid INT, north FLOAT, east FLOAT, south FLOAT, west FLOAT)
+RETURNS SETOF raster_values AS $$
+DECLARE
+  poly GEOMETRY;
 BEGIN
+  poly := polygon_from_bbox(north, east, south, west);
   RETURN QUERY
   SELECT (ST_DumpValues(r.rast)).*
   FROM grid_data AS r
-  WHERE meta_id=mid AND var_id=vid;
+  WHERE meta_id=mid
+        AND var_id=vid
+        AND ST_Intersects(
+          rast, poly);
 END;
 $$ LANGUAGE plpgsql;
