29010ff8f0c9a29e613fb85f612f72a027d7a715
legendOfZelda <sev.thaler@gmail.com>
2016-03-25 16:51:04 -0500

    testing extraction + json generation

--- ede/extract/extract.py
+++ ede/extract/extract.py
@@ -15,9 +15,11 @@ def return_all_metadata():
         new_doc['filename'] = row[0]
         new_doc['filesize'] = row[1]
         new_doc['filetype'] = row[2]
-        print type(row[3])
-        print row[3]
-        new_doc['meta_data'] = datetime.strftime(row[3])
+        #print type(row[3])
+        #print row[3]
+        #new_doc['meta_data'] = datetime.strftime(row[3])
+        print row[4]
+        print type(row[4])
         new_doc['date_created'] = row[4]
         new_doc['date_inserted'] = row[5]
         #print new_doc
