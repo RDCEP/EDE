bdbc53a8386e52c4b537b16459d52ffe1f1e5267
legendOfZelda <sev.thaler@gmail.com>
2016-03-25 16:27:29 -0500

    testing extraction + json generation

--- ede/extract/extract.py
+++ ede/extract/extract.py
@@ -1,15 +1,26 @@
 import psycopg2
 from ede.credentials import DB_NAME, DB_PASS, DB_PORT, DB_USER, DB_HOST
+import json
 
 conn = psycopg2.connect(database=DB_NAME, user=DB_USER, password=DB_PASS,
                             host=DB_HOST, port=DB_PORT)
 cur = conn.cursor()
 
 def return_all_metadata():
-    cur.execute("select filename, filesize from grid_meta")
+    cur.execute("select filename, filesize, filetype, meta_data, date_created, date_inserted from grid_meta")
     rows = cur.fetchall()
+    output = {}
     for row in rows:
-        print row
+        new_doc = {}
+        new_doc['filename'] = row[0]
+        new_doc['filesize'] = row[1]
+        new_doc['filetype'] = row[2]
+        #new_doc['meta_data'] = row[3]
+        new_doc['date_created'] = row[4]
+        new_doc['date_inserted'] = row[5]
+        #print new_doc
+        output.update(new_doc)
+    print output
 
 def main():
     return_all_metadata()
