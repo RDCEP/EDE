86860457cf3b0d4fb33438deac98bd17bcf132e4
legendOfZelda <sev.thaler@gmail.com>
2016-03-25 18:33:31 -0500

    testing extraction + json generation

--- ede/extract/extract.py
+++ ede/extract/extract.py
@@ -75,12 +75,26 @@ def return_aggregate_polygon_fixed_time(meta_id, var_id, poly, time):
     stddev = res[3]
     min = res[4]
     max = res[5]
-    print count, sum, avg, stddev, min, max
+
 
 # Q4: compute average of var(t, lat, lon) over t in [t_0, t_1] + (lat, lon)
 # within some polygon (temporal average within some region) aka Map Algebra needed here
-def return_aggregate_time_within_polygon():
-    print "todo"
+def return_aggregate_time_within_polygon(meta_id, var_id, poly, start_time, end_time):
+    poly_str = "ST_Polygon(ST_GeomFromText('LINESTRING(%s %s, %s %s, %s %s, %s %s, %s %s)'), 4326)" %\
+              (poly[0][0], poly[0][1], poly[1][0], poly[1][1], poly[2][0], poly[2][1], poly[3][0], poly[3][1], poly[4][0], poly[4][1])
+    tmp = "with foo as (select array(select ROW(ST_Union(ST_Clip(rast, %s)), 1)::rastbandarg as rast from grid_data " \
+            "where meta_id=%s and var_id=%s and time>='%s' and time<='%s' group by time))" %\
+          (poly_str, meta_id, var_id, start_time, end_time)
+    query = tmp + '\n' + "SELECT ST_X(geom), ST_Y(geom), val FROM " \
+            "(select (ST_PixelAsCentroids(ST_MapAlgebra((select * from foo)::rastbandarg[], " \
+            "'st_stddev4ma(double precision[], int[], text[])'::regprocedure))).*) foo;"
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        lon = row[0]
+        lat = row[1]
+        val = row[2]
+        print lon, lat, val
 
 
 # Q5: return all bands from a raster
@@ -111,10 +125,15 @@ def main():
     var_id = 1
     poly = [[-180, -90], [180, -90], [180, 90], [-180, 90], [-180, -90]]
     time = '1999-12-27 00:00:00-06'
-    return_aggregate_polygon_fixed_time(meta_id, var_id, poly, time)
+    #return_aggregate_polygon_fixed_time(meta_id, var_id, poly, time)
 
     # Q4
-
+    meta_id = 1
+    var_id = 1
+    poly = [[-180, -90], [180, -90], [180, 90], [-180, 90], [-180, -90]]
+    start_time = '1999-12-27 00:00:00-06'
+    end_time = '2016-12-27 00:00:00-06'
+    return_aggregate_time_within_polygon(meta_id, var_id, poly, start_time, end_time)
 
 if __name__ == "__main__":
     main()
