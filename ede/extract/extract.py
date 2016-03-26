e9eb0583303eca68fe5bb45c7518de1b9b8412d7
legendOfZelda <sev.thaler@gmail.com>
2016-03-25 19:41:22 -0500

    testing extraction + json generation

--- ede/extract/extract.py
+++ ede/extract/extract.py
@@ -102,15 +102,17 @@ def return_all_frames(meta_id, var_id):
     tmp = "with foo as (select st_astext((ST_PixelAsCentroids(rast)).geom) as pos, time, " \
             "(ST_PixelAsCentroids(rast)).val as val from grid_data where meta_id=%s and var_id=%s)" %\
           (meta_id, var_id)
-    query = tmp + '\n' + "select ST_X(pos), ST_Y(pos), array_to_string(array_agg((time, val)), ',') from foo group by foo.pos;"
+    query = tmp + '\n' + "select ST_X(pos), ST_Y(pos), array_to_string(array_agg((time, val)), ';') from foo group by foo.pos;"
     cur.execute(query)
     rows = cur.fetchall()
     counter = 0
     for row in rows:
         lon = row[0]
         lat = row[1]
-        vals = row[2]
-        print vals
+        vals = row[2].split(';')
+        for val in vals:
+            print val[0], type(val[0])
+            print val[1], type(val[1])
         counter += 1
         if counter == 1:
             break
