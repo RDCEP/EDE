2ebfbb3712602e913db15dbc29d1f4a6e7ed4cea
legendOfZelda <sev.thaler@gmail.com>
2016-03-30 18:21:28 -0500

    testing ingestion of shapefiles

--- ede/ingest/ingest_shapes.py
+++ ede/ingest/ingest_shapes.py
@@ -6,8 +6,11 @@ def main(shapefile):
     reader = ogr.Open(shapefile)
     layer = reader.GetLayer(0)
     for i in range(layer.GetFeatureCount()):
-        feature = layer.GetFeature(i)
-        print feature.ExportToJson()
+        feature = layer.GetFeature(i).ExportToJson()
+        print "geometry..."
+        print feature['geometry']['coordinates']
+        print "properties..."
+        print feature['properties']
 
 if __name__ == "__main__":
     shapefile = sys.argv[1]
