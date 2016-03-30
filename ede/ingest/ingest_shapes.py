986b43048a438e299d436d81101173feb51d9130
legendOfZelda <sev.thaler@gmail.com>
2016-03-30 18:38:55 -0500

    testing ingestion of shapefiles

--- ede/ingest/ingest_shapes.py
+++ ede/ingest/ingest_shapes.py
@@ -22,12 +22,10 @@ def main(shapefile):
     layer = reader.GetLayer(0)
     for i in range(layer.GetFeatureCount()):
         feature = layer.GetFeature(i).ExportToJson(as_object=True)
-        print feature
-        print type(feature)
         print "geometry..."
-        #print feature['geometry']['coordinates']
+        print feature['geometry']['coordinates']
         print "properties..."
-        #print feature['properties']
+        print feature['properties']
 
     '''
     cur.execute("insert into regions_meta (name, attributes) values (%s, %s) returning uid" % ())
