d7b16e0ffbb86557ebe89796d5c4152d74e25cc0
legendOfZelda <sev.thaler@gmail.com>
2016-03-31 16:01:51 -0500

    testing ingestion of shapefiles

--- ede/ingest/ingest_shapes.py
+++ ede/ingest/ingest_shapes.py
@@ -47,13 +47,11 @@ def main(shapefile):
         geom_str += " "
         geom_str += str(pts[num_pts-1][1]) # latitude
         geom_str = geom_str + "))"
-        meta_data = feature['properties']
-        meta_data = json.dumps(meta_data)
-        print meta_data
+        meta_data = json.dumps(feature['properties'])
         # (2) Ingest the feature with its geom + meta_data into the regions table
         query = "insert into regions (meta_id, geom, meta_data) values (%s, ST_GeomFromText(\'%s\'), \'%s\')" % (meta_id, geom_str, meta_data)
-        print query
         cur.execute(query)
+        print geom_str
 
     conn.commit()
 
