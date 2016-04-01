34d5fbdad8c8cf532dbfb69cc4be215e02003ee5
legendOfZelda <sev.thaler@gmail.com>
2016-03-31 20:24:27 -0500

    testing ingestion of shapefiles

--- ede/ingest/ingest_shapes.py
+++ ede/ingest/ingest_shapes.py
@@ -38,38 +38,54 @@ def main(shapefile):
         depth = depth_fnc(geom)
         # The case of non-multi-polygons
         if depth == 3:
-            geom_str = "POLYGON(("
-            for ring in geom:
-                for pt in ring:
-                    print ' '.join(map(str, pt))
+            geom_str = "POLYGON("
+            num_rings = len(geom)
+            for i_ring in range(num_rings):
+                geom_str += '('
+                ring = geom(i_ring)
+                num_pts = len(ring)
+                for i_pt in range(num_pts):
+                    geom_str += ' '.join(map(str, ring[i_pt]))
+                    if i_pt < num_pts-1:
+                        geom_str += ','
+                geom_str += ')'
+                if i_ring < num_rings-1:
+                    geom_str += ','
+            geom_str += ')'
+
+            print geom_str
+
         # The case of multi-polygons
         elif depth == 4:
-            geom_str = "MULTIPOLYGON(("
-            for poly in geom:
-                for ring in poly:
-                    for pt in ring:
-                        print ' '.join(map(str, pt))
+            geom_str = "MULTIPOLYGON("
+            num_polys = len(geom)
+            for i_poly in range(num_polys):
+                geom_str += '('
+                poly = geom(i_poly)
+                num_rings = len(poly)
+                for i_ring in range(num_rings):
+                    geom_str += '('
+                    ring = poly(i_ring)
+                    num_pts = len(ring)
+                    for i_pt in range(num_pts):
+                        geom_str += ' '.join(map(str, ring[i_pt]))
+                        if i_pt < num_pts-1:
+                            geom_str += ','
+                    geom_str += ')'
+                    if i_ring < num_rings-1:
+                        geom_str += ','
+                geom_str += ')'
+                if i_poly < num_polys-1:
+                    geom_str += ','
+            geom_str += ')'
+
+            print geom_str
+
         else:
             sys.exit("got unexpected nestedness depth of %s in feature" % depth)
 
-
-
-        for p in range(num_pts-1):
-            #print "next point: %s" % pts[p]
-            geom_str += str(pts[p][0]) # longitude
-            geom_str += " "
-            geom_str += str(pts[p][1]) # latitude
-            geom_str += ", "
-        geom_str += str(pts[num_pts-1][0]) # longitude
-        geom_str += " "
-        geom_str += str(pts[num_pts-1][1]) # latitude
-        geom_str = geom_str + "))"
-
         meta_data = json.dumps(feature['properties'])
-
         # (2) Ingest the feature with its geom + meta_data into the regions table
-        #print geom
-        #print geom_str
         query = "insert into regions (meta_id, geom, meta_data) values (%s, ST_GeomFromText(\'%s\'), \'%s\')" % (meta_id, geom_str, meta_data)
         cur.execute(query)
 
