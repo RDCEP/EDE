391a08db6ae2999e0f7031547d8d4451c6bcab95
legendOfZelda <sev.thaler@gmail.com>
2016-04-01 19:48:15 -0500

    adapting queries to new schema

--- ede/extract/extract.py
+++ ede/extract/extract.py
@@ -163,7 +163,7 @@ def return_aggregate_time_within_polygon(meta_id, var_id, poly, dates):
     poly_str = "ST_Polygon(ST_GeomFromText('LINESTRING(%s %s, %s %s, %s %s, %s %s, %s %s)'), 4326)" %\
               (poly[0][0], poly[0][1], poly[1][0], poly[1][1], poly[2][0], poly[2][1], poly[3][0], poly[3][1], poly[4][0], poly[4][1])
     date_str = '(' + ','.join(map(str, dates)) + ')'
-    print date_str
+    #print date_str
     tmp = "with foo as (select array(select ROW(ST_Union(ST_Clip(rast, %s)), 1)::rastbandarg as rast from grid_data " \
             "where meta_id=%s and var_id=%s and date in %s group by date))" %\
           (poly_str, meta_id, var_id, date_str)
@@ -179,7 +179,6 @@ def return_aggregate_time_within_polygon(meta_id, var_id, poly, dates):
     out['response']['status'] = 'OK'
     out['response']['status_code'] = 200
     out['response']['metadata'] = {}
-    out['response']['metadata']['timesteps'] = [start_date, end_date]
     out['response']['metadata']['units'] = 'TKTK'
     out['response']['metadata']['format'] = 'grid'
     out['response']['data'] = []
@@ -195,6 +194,12 @@ def return_aggregate_time_within_polygon(meta_id, var_id, poly, dates):
         new_data_item['geometry'] = {'type': 'Point', 'coordinates': [lon, lat]}
         new_data_item['properties'] = {'values': [val]}
         out['response']['data'].append(new_data_item)
+    query = "select to_char(date, \'YYYY-MM-DD HH24:MI:SS\') from grid_dates where uid in %s" % date_str
+    rows = db_session.execute(query)
+    out['response']['metadata']['timesteps'] = []
+    for row in rows:
+        date_str = str(row[0])
+        out['response']['metadata']['timesteps'].append(date_str)
     return out
 
 
