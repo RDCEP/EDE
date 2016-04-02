f136ddd52a7fac2f6617d4d108552254b0949d93
legendOfZelda <sev.thaler@gmail.com>
2016-04-02 11:43:01 -0500

    testing basic flask api

--- ede/api/views.py
+++ ede/api/views.py
@@ -5,9 +5,8 @@ try:
 except ImportError:
     import json
 from datetime import date
-from flask import Blueprint, make_response
+from flask import Blueprint, make_response, request
 from flask.ext.cache import Cache
-from ede.schema.models import Grid_Meta
 from ede.config import CACHE_CONFIG
 from ede.extract.extract import *
 
@@ -18,7 +17,6 @@ RESPONSE_LIMIT = 1000
 CACHE_TIMEOUT = 60 * 60 * 6
 
 api = Blueprint('ede_api', __name__, url_prefix='/api/{}'.format(API_VERSION))
-
 
 dthandler = lambda obj: obj.isoformat() if isinstance(obj, date) else None
 
@@ -36,17 +34,6 @@ def flush_cache():
 @api.route('/gridmeta', defaults={'ids': None}, methods=['GET'])
 @api.route('/gridmeta/<intlist:ids>', methods=['GET'])
 def get_gridmeta(ids):
-    """Get metadata from gridded datasets.
-
-    Passing no ids returns metadata for all datasets. A comma-separated list
-    of ids returns metadata for multiple datasets. A single integer id
-    returns metadata for a single dataset.
-
-    If ids is empty, filter results by parameters in the body of the request.
-
-    :param ids:
-    :return:
-    """
     status_code = 200
     data = return_all_metadata(ids)
     resp = make_response(json.dumps(data, default=dthandler), status_code)
@@ -54,84 +41,37 @@ def get_gridmeta(ids):
     return resp
 
 
-# QUERY 1
-@api.route('/griddata/point/<int:meta_id>/<int:var_id>/<string:poly>/<string:time>', methods=['GET'])
-def get_griddata_by_points(meta_id, var_id, poly, time):
-    print meta_id, var_id, poly, time
-    poly = poly.split(';')
-    poly = [ pt.split(',') for pt in poly]
-    print poly
-
-
-# QUERY 2
-@api.route('/griddata/tile/<int:meta_id>/<int:var_id>/<string:poly>/<string:time>', methods=['GET'])
-def get_griddata_by_tile(meta_id, var_id, poly, time):
-    print meta_id, var_id, poly, time
+# QUERY 1 + 5
+@api.route('/griddata/select/<int:meta_id>/<int:var_id>/<int:poly>/<int:date>', methods=['GET'])
+def get_griddata_by_points(meta_id, var_id, poly, date):
+    status_code = 200
+    data = return_within_region_fixed_time(meta_id, var_id, poly, date)
+    resp = make_response(json.dumps(data, default=dthandler), status_code)
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
 
 
 # QUERY 3
-@api.route('/griddata/aggregate/spatial/<int:meta_id>/<int:var_id>/<string:poly>/<string:time>', methods=['GET'])
-def get_spatial_aggregation(meta_id, var_id, poly, t):
-    """Get spatial aggregation of gridded data within some polygon.
-
-    :return:
-    """
+@api.route('/griddata/aggregate/spatial/<int:meta_id>/<int:var_id>/<int:poly>/<int:date>', methods=['GET'])
+def get_spatial_aggregation(meta_id, var_id, poly, date):
     status_code = 200
-    data = return_aggregate_polygon_fixed_time(meta_id, var_id, poly, t)
+    data = return_aggregate_polygon_fixed_time(meta_id, var_id, poly, date)
     resp = make_response(json.dumps(data, default=dthandler), status_code)
     resp.headers['Content-Type'] = 'application/json'
     return resp
 
 
 # QUERY 4
-@api.route('/griddata/aggregate/temporal/<int:meta_id>/<int:var_id>/<string:poly>/<string:start_time>/<string:end_time>', methods=['GET'])
-def get_temporal_aggregation(meta_id, var_id, poly, start_time, end_time):
-    """Get temporal aggregation of gridded data over some time interval.
-
-    :return:
-    """
+@api.route('/griddata/aggregate/temporal/<int:meta_id>/<int:var_id>/<int:poly>/<intlist:dates>', methods=['GET'])
+def get_temporal_aggregation(meta_id, var_id, poly, dates):
     status_code = 200
-    data = return_aggregate_time_within_polygon(meta_id, var_id, poly, start_time, end_time)
+    data = return_aggregate_time_within_polygon(meta_id, var_id, poly, dates)
     resp = make_response(json.dumps(data, default=dthandler), status_code)
     resp.headers['Content-Type'] = 'application/json'
     return resp
 
 
-# QUERY 5
-@api.route('/griddata/<int:meta_id>/<int:var_id>', methods=['GET'])
-def get_griddata(meta_id, var_id):
-    """Get gridded dataset by its dataset id + variable id.
-
-    Note: Because the `id` parameter is converted to an `int`, you can only
-    get one dataset at a time.
-
-    :param meta_id:
-    :param var_id:
-    :return:
-    """
-    status_code = 200
-    data = return_all_frames(meta_id, var_id)
-    resp = make_response(json.dumps(data, default=dthandler), status_code)
-    resp.headers['Content-Type'] = 'application/json'
-    return resp
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+'''
 @api.route('/polymeta/<list:ids>', defaults={'ids': None}, methods=['GET'])
 def get_polymeta(ids):
     """Get metadata for sets of polygonal regions.
@@ -159,3 +99,4 @@ def get_polydata(id):
     :return:
     """
     pass
+'''
