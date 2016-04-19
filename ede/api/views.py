4682bbbae4da8ef969faea0cce5d1554c5012c49
legendOfZelda <sev.thaler@gmail.com>
2016-04-19 08:35:58 -0500

    use path instead of url_rule

--- ede/api/views.py
+++ ede/api/views.py
@@ -42,7 +42,7 @@ def get_gridmeta(ids):
     """
     status_code = 200
     data = return_gridmeta(ids)
-    data['request']['url'] = request.route
+    data['request']['url'] = request.path
     resp = make_response(json.dumps(data, default=dthandler), status_code)
     resp.headers['Content-Type'] = 'application/json'
     return resp
