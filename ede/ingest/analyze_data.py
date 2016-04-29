27aac230bc311dd1695965b1239a31ed3a6f5af1
legendOfZelda <sev.thaler@gmail.com>
2016-04-29 13:21:24 -0500

    analyze data script

--- ede/ingest/analyze_data.py
+++ ede/ingest/analyze_data.py
@@ -4,14 +4,20 @@ import numpy as np
 
 
 def main(netcdf_filename):
+
     rootgrp = Dataset(netcdf_filename, "r", format="NETCDF4")
 
-    lons = rootgrp.variables['lon'][:]
-    lats = rootgrp.variables['lat'][:]
-    yield_whe = rootgrp.variables['yield_whe'][:]
-    print "number of longitudes: %d" % lons.size
-    print "number of latitudes: %d" % lats.size
-    print "number of yield_whe's: %d" % yield_whe.size
+    # lons = rootgrp.variables['lon'][:]
+    # lats = rootgrp.variables['lat'][:]
+    time = rootgrp.variables['time']
+
+    for t in range(time.size):
+        yield_whe_frame = rootgrp.variables['yield_whe'][t]
+        print yield_whe_frame.size
+
+    # print "number of longitudes: %d" % lons.size
+    # print "number of latitudes: %d" % lats.size
+    # print "number of yield_whe's: %d" % yield_whe.size
 
     rootgrp.close()
 
