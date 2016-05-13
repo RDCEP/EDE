4434b5b943cea75c0e4bacc465690df7f2294ef5
legendOfZelda <sev.thaler@gmail.com>
2016-05-13 09:58:51 -0500

    working on simplified ingestion script

--- ede/ingest/simplify_ingest/ingest_netcdf.py
+++ ede/ingest/simplify_ingest/ingest_netcdf.py
@@ -149,6 +149,7 @@ def process_band(band, band_dim):
     num_tiles_lon = math.ceil(band_shape[1] / tile_size_lon)
     for i in range(num_tiles_lat):
         for j in range(num_tiles_lon):
+            print("value: {}".format(band[0][0]))
             tile = band[i*tile_size_lat : (i+1)*tile_size_lat][j*tile_size_lon : (j+1)*tile_size_lon]
             print("begin processing of tile...")
             process_tile(tile, band_dim)
