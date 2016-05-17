5acc30c3673c4967aa88cc7cc5a38741b7db2784
legendOfZelda <sev.thaler@gmail.com>
2016-05-17 16:05:33 -0500

    debugging new ingestion script

--- ede/ingest/simplify_ingest/ingest_netcdf.py
+++ ede/ingest/simplify_ingest/ingest_netcdf.py
@@ -27,7 +27,7 @@ def insert_get_var_id(cursor, variable):
             return var_id
     except DatabaseError as e:
         eprint(e)
-        raise RasterProcessingException("Could not get variable id for variable: {}. Due to DatabaseError"
+        raise RasterProcessingException("Could not get variable id for variable: {}. Due to DatabaseError!"
                                         .format(variable.name))
     except Exception as e:
         eprint(e)
