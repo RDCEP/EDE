5f19ef519504327511b53bd4174824094d42e53f
legendOfZelda <sev.thaler@gmail.com>
2016-06-22 09:00:39 -0600

    added template json

---
+++ ede/extract/templatejson/generate_template_json.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python 
+
+import json
+
+num_lons = 720
+num_lats = 360
+lon_start = -179.75
+lon_step = 0.5
+lat_start = 89.75
+lat_step = -0.5
+
+with open('template.json', 'w') as outfile:
+	res = {}
+
+	res['request'] = {}
+	res['request']['url'] = '/api/v0/griddata/dataset/1/var/1/time/1'
+	
+	res['response'] = {}
+	res['response']['status'] = 'OK'
+	res['response']['status_code'] = 200
+	
+	res['response']['metadata'] = {}
+	res['response']['metadata']['format'] = 'grid'
+		
+	res['response']['data'] = []
+	lat_c = lat_start
+	for i in range(num_lats):
+		lon_c = lon_start
+		for j in range(num_lons):
+			new_pt = {}
+			new_pt['type'] = 'Feature'
+			new_pt['geometry'] = {'type': 'Point', 'coordinates': [lon_c, lat_c]}
+			res['response']['data'].append(new_pt)
+			lon_c += lon_step
+		lat_c += lat_step
+	json.dump(res, outfile)
