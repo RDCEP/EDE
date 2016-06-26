b7781fde06d905b4280faff16916a2b9bdb48fba
legendOfZelda <sev.thaler@gmail.com>
2016-06-26 13:10:01 -0600

    spedup ingestion of jsons

--- ede/ingest/new_json_schema/ingest_netcdf_as_json.py
+++ ede/ingest/new_json_schema/ingest_netcdf_as_json.py
@@ -7,9 +7,7 @@ import json
 
 dataset_id = 1
 filename = sys.argv[1]
-#filename = '/data/psims/papsim_wfdei.cru_hist_fullharm_firr_yield_whe_annual_1979_2012.nc4'
 var_id = int(sys.argv[2])
-#var_id = 1
 
 fh = Dataset(filename)
 
@@ -23,15 +21,14 @@ for var in fh.variables.values():
 
 values = fh.variables[var_name][:]
 
-outfile = open('out_file.sql', 'w')
-
-for slice_id, slice in enumerate(values):
-	time_id = slice_id+1
-	for (lat_id, lon_id), value_tmp in np.ndenumerate(slice):
-		value = float(value_tmp)
-		if value_tmp == slice.get_fill_value():
-			value = None
-		lat = lats[lat_id]
-		lon = lons[lon_id]
-		json_data = {'type': 'Feature', 'geometry': {'type': 'Point', 'coordinates': [lon, lat]}, 'properties': {'values': [value]}}
-		outfile.write('INSERT INTO grid_data(dataset_id, var_id, time_id, geom, json) VALUES ({}, {}, {}, ST_GeomFromText(\'POINT({} {})\', 4326), \'{}\');\n'.format(dataset_id, var_id, time_id, lon, lat, json.dumps(json_data)))
+with open('out.csv', 'w') as f:
+	for slice_id, slice in enumerate(values):
+		time_id = slice_id+1
+		for (lat_id, lon_id), value_tmp in np.ndenumerate(slice):
+			value = float(value_tmp)
+			if value_tmp == slice.get_fill_value():
+				value = "null"
+			lat = lats[lat_id]
+			lon = lons[lon_id]
+			json_data = "{{\"type\": \"Feature\", \"geometry\": {{\"type\": \"Point\", \"coordinates\": [{0}, {1}]}}, \"properties\": {{\"values\": [{2}]}}}}".format(lon, lat, value)
+			f.write("{}\t{}\t{}\tSRID=4326;POINT({} {})\t{}\n".format(dataset_id, var_id, time_id, lon, lat, json_data))
