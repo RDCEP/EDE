be68d10c890ac7501b93590c21ef9ab7e0e8a3dc
legendOfZelda <sev.thaler@gmail.com>
2016-10-22 14:54:04 -0500

    readded truncation of last newline to ingestion script

--- ede/ingest/rasters/psims/ingest_netcdf.py
+++ ede/ingest/rasters/psims/ingest_netcdf.py
@@ -641,6 +641,8 @@ def process_netcdf(netcdf_filename, wkb_filename):
                         hexwkb = rast.raster_to_hexwkb(1)
                         row = compose_fields(dataset_id, var_id, None, hexwkb)
                         f.write(row + '\n')
+                f.seek(-1, os.SEEK_END)
+                f.truncate()
                 ingest_actual_data(wkb_filename, cur, var)
                 print("size of CSV file: {}".format(os.path.getsize(wkb_filename)))
 
